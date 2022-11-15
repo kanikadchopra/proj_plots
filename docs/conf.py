d91f60c7bdd992a477a21c86f01127a79f83a694
mlysy <mlysy@users.noreply.github.com>
2022-11-14 23:18:07 -0500

    updated homepage information

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Kanika Chopra, Martin Lysy'
 copyright = '2022, ' + author
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
